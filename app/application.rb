c27d2d13ddef8ebd2da361ee4d817e26e33a40b6
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-28 05:02:56 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+#binding.pry
+    if req.path.match("/items")
+      item_name = req.path.split("/items/").last
+      #req.path => "/items/Figs", req.path.split("/items/") => ["", "Figs"]
+      
+      @@items.each do |item|
+        if item.name = item_name
+          resp.write "#{item.price}\n"
+          resp.status = 200
+        else
+          resp.write "Item not found\n"
+          resp.status = 400
+        end
+      end
+      
+    else
+      resp.write "Route not found\n"
+      resp.status = 404
+    end
+    resp.finish
+  end 
+end 
